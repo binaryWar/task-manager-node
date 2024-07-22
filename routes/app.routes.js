a56a969a6f0a7f708a7da27e1c3c0ca712feb77e
binaryWar <ashwk03@gmail.com>
2024-07-22 22:47:10 +0530

    google-auth

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const taskRoutes = require("./task.routes");
 const userRoutes = require("./user.routes");
+const googleAuthRoutes = require("../services/auth");
 
 router.use('/user',userRoutes);
 router.use('/task',taskRoutes);
---
+++ services/auth.js
@@ -0,0 +1,54 @@
+// auth.js
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
+const router = express.Router();
+
+const app = express();
+
+// Configure cookie session
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: '598027431043-ufr8mpvkoi25qkt9hnkvq17rvr4b3dhg.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: 'http://localhost:3000/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+  // You can save user information to your database here
+  done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    console.log("hello and the my name is khan ")
+    res.redirect('http://localhost:4200');
+  }
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('http://localhost:4200');
+});
+
+module.exports = app;
